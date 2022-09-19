c1f2ac6c1f49e37d9416b928d248642f5d00284b
Mahejabeen Naib <mahejabeen.naib@publicissapient.com>
2022-09-19 10:08:24 +0530

    added all data

--- pages_k/index.js
+++ pages_k/index.js
@@ -2,10 +2,9 @@ import Head from "next/head";
 import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
 import { getSortedPostsData } from "../lib/posts";
-import Link from "next/link";
 
 const Home = ({ allPostsData, hi }) => {
-  console.log(allPostsData);
+  console.log(hi);
 
   return (
     <Layout home>
@@ -29,11 +28,11 @@ const Home = ({ allPostsData, hi }) => {
           {allPostsData &&
             allPostsData.map(({ id, date, title }) => (
               <li className={utilStyles.listItem} key={id}>
-                <Link href={`/posts/${id}`}>
-                  <a>{title}</a>
-                </Link>
+                {title}
                 <br />
-                <small className={utilStyles.lightText}>{date}</small>
+                {id}
+                <br />
+                {date}
               </li>
             ))}
         </ul>
